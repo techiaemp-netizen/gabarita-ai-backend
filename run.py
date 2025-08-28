7d63301dc4f8a93e65227ded298ee0164358e3e7
techiaemp-netizen <techia.emp@gmail.com>
2025-08-28 08:17:11 -0300

    Deploy completo 2025: Backend com correções e melhorias implementadas

--- run.py
+++ run.py
@@ -1,17 +1,4 @@
-#!/usr/bin/env python3
-"""
-Script para executar o servidor Flask
-"""
-
-import sys
-import os
-
-# Adicionar o diretório src ao path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-
-# Importar e executar o app
 from src.main import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
